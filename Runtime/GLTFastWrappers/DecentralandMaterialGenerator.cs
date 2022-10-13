b892ce9c596d494a34e8edd101f5cab3c21f45f6
Kinerius <kinerius@gmail.com>
2022-10-13 15:33:36 -0300

    decoupled gltf wrappers

---
+++ Runtime/GLTFastWrappers/DecentralandMaterialGenerator.cs
@@ -0,0 +1,256 @@
+﻿using DCL.Helpers;
+using DCL.Shaders;
+using GLTFast;
+using GLTFast.Materials;
+using GLTFast.Schema;
+using UnityEngine;
+using UnityEngine.Rendering;
+using GLTFastMaterial = GLTFast.Schema.Material;
+using Material = UnityEngine.Material;
+
+namespace DCL.GLTFast.Wrappers
+{
+    public class DecentralandMaterialGenerator : MaterialGenerator
+    {
+        // Historically we have no data on why we have this intensity
+        private const float EMISSIVE_HDR_INTENSITY = 5f;
+
+        private Material material;
+        private readonly Shader shader;
+
+        public DecentralandMaterialGenerator(string shaderName) { shader = Shader.Find(shaderName); }
+        public override Material GetDefaultMaterial() { return new Material(shader); }
+        
+        /// <summary>
+        /// Here we convert a GLTFMaterial into our Material using our shaders
+        /// </summary>
+        /// <param name="gltfMaterial"></param>
+        /// <param name="gltf"></param>
+        /// <returns></returns>
+        public override Material GenerateMaterial(GLTFastMaterial gltfMaterial, IGltfReadable gltf)
+        {
+            material = GetDefaultMaterial();
+            material.name = gltfMaterial.name;
+
+            if (gltfMaterial.extensions?.KHR_materials_pbrSpecularGlossiness != null)
+            {
+                var specGloss = gltfMaterial.extensions.KHR_materials_pbrSpecularGlossiness;
+
+                SetColor(specGloss.diffuseColor);
+                SetSpecularColor(specGloss.specularColor);
+                SetGlossiness(specGloss.glossinessFactor);
+                SetBaseMapTexture(specGloss.diffuseTexture, gltf);
+                SetSpecularMapTexture(specGloss.specularGlossinessTexture, gltf);
+            }
+            // If there's a specular-glossiness extension, ignore metallic-roughness
+            // (according to extension specification)
+            else
+            {
+                PbrMetallicRoughness roughness = gltfMaterial.pbrMetallicRoughness;
+
+                if (roughness != null)
+                {
+                    SetColor(roughness.baseColor);
+                    SetBaseMapTexture(roughness.baseColorTexture, gltf);
+                    SetMetallic(roughness.metallicFactor);
+                    SetMetallicRoughnessTexture( gltf, roughness.metallicRoughnessTexture, roughness.roughnessFactor);
+                }
+            }
+
+            SetBumpMapTexture( gltfMaterial.normalTexture, gltf);
+            SetOcclusionTexture(  gltfMaterial.occlusionTexture, gltf);
+            SetEmissiveColor( gltfMaterial.emissive);
+            SetEmissiveTexture(  gltfMaterial.emissiveTexture, gltf);
+
+            SetAlphaMode(gltfMaterial.alphaModeEnum, gltfMaterial.alphaCutoff);
+            SetDoubleSided(gltfMaterial.doubleSided);
+            
+            SRPBatchingHelper.OptimizeMaterial(material);
+            
+            return material;
+        }
+        
+        private void SetEmissiveColor( Color gltfMaterialEmissive)
+        {
+            if (gltfMaterialEmissive != Color.black)
+            {
+                material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+                material.SetColor(ShaderUtils.EmissionColor, gltfMaterialEmissive * EMISSIVE_HDR_INTENSITY);
+                material.EnableKeyword(ShaderUtils.KEYWORD_EMISSION);
+            }
+        }
+        
+        private void SetEmissiveTexture(TextureInfo emissiveTexture, IGltfReadable gltf)
+        {
+            if (TrySetTexture(
+                    emissiveTexture,
+                    material,
+                    gltf,
+                    ShaderUtils.EmissionMap,
+                    ShaderUtils.EmissionMapRotation,
+                    ShaderUtils.EmissionMapScaleTransform,
+                    ShaderUtils.EmissionMapUVChannel
+                ))
+            {
+                material.SetInt(ShaderUtils.EmissiveMapUVs, emissiveTexture.texCoord);
+                material.EnableKeyword(ShaderUtils.KEYWORD_EMISSION);
+            }
+        }
+        
+        private void SetOcclusionTexture(OcclusionTextureInfo occlusionTexture, IGltfReadable gltf)
+        {
+            if (TrySetTexture(
+                    occlusionTexture,
+                    material,
+                    gltf,
+                    ShaderUtils.OcclusionMap,
+                    ShaderUtils.OcclusionMapRotation,
+                    ShaderUtils.OcclusionMapScaleTransform,
+                    ShaderUtils.OcclusionMapUVChannel
+                ))
+            {
+                material.EnableKeyword(ShaderUtils.KEYWORD_OCCLUSION);
+                material.SetFloat(ShaderUtils.OcclusionStrength, occlusionTexture.strength);
+            }
+        }
+        
+        private void SetBumpMapTexture(NormalTextureInfo textureInfo, IGltfReadable gltf)
+        {
+            if (TrySetTexture(
+                    textureInfo,
+                    material,
+                    gltf, 
+                    ShaderUtils.BumpMap,
+                    ShaderUtils.BumpMapScaleTransformPropId,
+                    ShaderUtils.BumpMapRotationPropId,
+                    ShaderUtils.BumpMapUVChannelPropId
+                ))
+            {
+                material.SetInt(ShaderUtils.NormalMapUVs, textureInfo.texCoord);
+                material.SetFloat(ShaderUtils.BumpScale, textureInfo.scale);
+                material.EnableKeyword(ShaderUtils.KEYWORD_NORMALMAP);
+            }
+        }
+
+        private void SetMetallicRoughnessTexture(IGltfReadable gltf, TextureInfo textureInfo, float roughnessFactor)
+        {
+            if (TrySetTexture(
+                    textureInfo,
+                    material,
+                    gltf,
+                    ShaderUtils.MetallicRoughnessMapPropId,
+                    ShaderUtils.MetallicRoughnessMapScaleTransformPropId,
+                    ShaderUtils.MetallicRoughnessMapRotationPropId,
+                    ShaderUtils.MetallicRoughnessMapUVChannelPropId
+                ))
+            {
+                SetSmoothness(1);
+                material.SetInt(ShaderUtils.MetallicMapUVs, textureInfo.texCoord);
+                material.EnableKeyword(ShaderUtils.KEYWORD_METALLICSPECGLOSSMAP);
+            }
+            else
+            {
+                SetSmoothness(1 - roughnessFactor);
+            }
+        }
+
+        private void SetSmoothness(float roughnessFactor) { material.SetFloat(ShaderUtils.Smoothness, roughnessFactor); }
+
+        private void SetMetallic(float metallicFactor) { material.SetFloat(ShaderUtils.Metallic, metallicFactor); }
+
+        private void SetSpecularMapTexture(TextureInfo textureInfo, IGltfReadable gltf)
+        {
+            if(TrySetTexture(
+                textureInfo,
+                material,
+                gltf,
+                ShaderUtils.SpecGlossMap,
+                ShaderUtils.SpecGlossMapScaleTransform,
+                ShaderUtils.SpecGlossMapRotation,
+                ShaderUtils.SpecGlossMapUVChannelPropId))
+            {
+                material.SetFloat(ShaderUtils.SmoothnessTextureChannel, 0);
+                material.EnableKeyword(ShaderUtils.KEYWORD_SPECGLOSSMAP);
+            }
+        }
+
+        private void SetBaseMapTexture(TextureInfo textureInfo, IGltfReadable gltf)
+        {
+            TrySetTexture(
+                textureInfo,
+                material,
+                gltf,
+                ShaderUtils.BaseMap,
+                ShaderUtils.BaseMapRotation,
+                ShaderUtils.BaseMapScaleTransform,
+                ShaderUtils.BaseMapUVs
+            );
+        }
+
+        private void SetSpecularColor(Color color) { material.SetVector(ShaderUtils.SpecColor, color); }
+
+        private void SetGlossiness(float glossiness)
+        {
+            material.SetFloat(ShaderUtils.GlossMapScale, glossiness);
+            material.SetFloat(ShaderUtils.Glossiness, glossiness);
+        }
+
+        private void SetColor(Color color) { material.SetColor(ShaderUtils.BaseColor, color); }
+
+        private void SetAlphaMode(GLTFastMaterial.AlphaMode alphaMode, float alphaCutoff)
+        {
+            switch (alphaMode)
+            {
+                case GLTFastMaterial.AlphaMode.MASK:
+                    material.SetOverrideTag(ShaderUtils.RENDERER_TYPE, "TransparentCutout");
+                    material.SetInt(ShaderUtils.SrcBlend, (int)BlendMode.One);
+                    material.SetInt(ShaderUtils.DstBlend, (int)BlendMode.Zero);
+                    material.SetInt(ShaderUtils.ZWrite, 1);
+                    material.SetFloat(ShaderUtils.AlphaClip, 1);
+                    material.EnableKeyword(ShaderUtils.KEYWORD_ALPHA_TEST);
+                    material.DisableKeyword(ShaderUtils.KEYWORD_ALPHA_PREMULTIPLY);
+                    material.renderQueue = (int)RenderQueue.AlphaTest;
+
+                    if (material.HasProperty(ShaderUtils.Cutoff))
+                        material.SetFloat(ShaderUtils.Cutoff, alphaCutoff);
+
+                    break;
+
+                case GLTFastMaterial.AlphaMode.BLEND:
+                    material.SetOverrideTag(ShaderUtils.RENDERER_TYPE, "Transparent");
+                    material.SetInt(ShaderUtils.SrcBlend, (int)BlendMode.SrcAlpha);
+                    material.SetInt(ShaderUtils.DstBlend, (int)BlendMode.OneMinusSrcAlpha);
+                    material.SetInt(ShaderUtils.ZWrite, 0);
+                    material.DisableKeyword(ShaderUtils.KEYWORD_ALPHA_TEST);
+                    material.DisableKeyword(ShaderUtils.KEYWORD_ALPHA_PREMULTIPLY);
+                    material.renderQueue = (int)RenderQueue.Transparent;
+                    material.SetFloat(ShaderUtils.Cutoff, 0);
+                    break;
+                default:
+                    material.SetOverrideTag(ShaderUtils.RENDERER_TYPE, "Opaque");
+                    material.SetInt(ShaderUtils.SrcBlend, (int)BlendMode.One);
+                    material.SetInt(ShaderUtils.DstBlend, (int)BlendMode.Zero);
+                    material.SetInt(ShaderUtils.ZWrite, 1);
+                    material.DisableKeyword(ShaderUtils.KEYWORD_ALPHA_TEST);
+                    material.DisableKeyword(ShaderUtils.KEYWORD_ALPHA_PREMULTIPLY);
+                    material.renderQueue = (int)RenderQueue.Geometry;
+                    material.SetFloat(ShaderUtils.Cutoff, 0);
+                    break;
+            }
+        }
+
+        private void SetDoubleSided(bool doubleSided)
+        {
+            if (doubleSided)
+            {
+                material.SetInt(ShaderUtils.Cull, (int)CullMode.Off);
+            }
+            else
+            {
+                material.SetInt(ShaderUtils.Cull, (int)CullMode.Back);
+            }
+
+            material.doubleSidedGI = doubleSided;
+        }
+    }
+}
---
+++ Runtime/Shaders/ShaderUtils.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+namespace DCL.Shaders
+{
+    public static class ShaderUtils
+    {
+        //Toon shader properties
+        public static readonly int LightDir = Shader.PropertyToID("_LightDir");
+        public static readonly int LightColor = Shader.PropertyToID("_LightColor");
+        public static readonly int TintColor = Shader.PropertyToID("_TintColor");
+
+        public static readonly int GlossMatCap = Shader.PropertyToID("_GlossMatCap");
+        public static readonly int FresnelMatCap = Shader.PropertyToID("_FresnelMatCap");
+        public static readonly int MatCap = Shader.PropertyToID("_MatCap");
+        public static readonly int DitherFade = Shader.PropertyToID("_DitherFade");
+
+        public static readonly int SpecularHighlights = Shader.PropertyToID("_SpecularHighlights");
+        public static readonly int EnvironmentReflections = Shader.PropertyToID("_EnvironmentReflections");
+        public static readonly int Surface = Shader.PropertyToID("_Surface");
+
+        //Avatar specific properties
+        public static readonly int EyesTexture = Shader.PropertyToID("_EyesTexture");
+        public static readonly int EyeTint = Shader.PropertyToID("_EyeTint");
+        public static readonly int IrisMask = Shader.PropertyToID("_IrisMask");
+        public static readonly int TintMask = Shader.PropertyToID("_TintMask");
+        public static readonly string SSAO_OFF_KEYWORD = "_SSAO_OFF";
+        
+        // keywords
+        public const string KEYWORD_OCCLUSION = "_OCCLUSION";
+        public const string KEYWORD_EMISSION = "_EMISSION";
+        public const string KEYWORD_ALPHA_PREMULTIPLY = "_ALPHAPREMULTIPLY_ON";
+        public const string KEYWORD_ALPHA_TEST = "_ALPHATEST_ON";
+        public const string KEYWORD_SPECGLOSSMAP = "_SPECGLOSSMAP";
+        public const string KEYWORD_NORMALMAP = "_NORMALMAP";
+        public const string KEYWORD_METALLICSPECGLOSSMAP = "_METALLICSPECGLOSSMAP";
+
+        public const string RENDERER_TYPE = "RenderType";
+
+        //Lit properties
+        public static readonly int AlphaTexture = Shader.PropertyToID("_AlphaTexture");
+        
+        // NOTE(Kinerius) All those Shader properties that are not supported are there in purpose since they are being
+        //                used by GLTFast shaders and might be specs from GLTF format that we have to support in the future
+        public static readonly int SpecGlossMap = Shader.PropertyToID("_SpecGlossMap");
+        public static readonly int SpecGlossMapScaleTransform = Shader.PropertyToID("_SpecGlossMap_ST"); // we dont support this yet
+        public static readonly int SpecGlossMapRotation = Shader.PropertyToID("_SpecGlossMapRotation"); // we dont support this yet
+        public static readonly int SpecGlossMapUVChannelPropId = Shader.PropertyToID("_SpecGlossMapUVChannel"); // we dont support this yet
+
+        public static readonly int MetallicRoughnessMapPropId = Shader.PropertyToID("_MetallicGlossMap");
+        public static readonly int MetallicRoughnessMapScaleTransformPropId = Shader.PropertyToID("metallicRoughnessTexture_ST"); // we dont support this yet
+        public static readonly int MetallicRoughnessMapRotationPropId = Shader.PropertyToID("metallicRoughnessTextureRotation"); // we dont support this yet
+        public static readonly int MetallicRoughnessMapUVChannelPropId = Shader.PropertyToID("metallicRoughnessTextureUVChannel"); // we dont support this yet
+
+        public static readonly int SmoothnessTextureChannel = Shader.PropertyToID("_SmoothnessTextureChannel");
+        public static readonly int SpecColor = Shader.PropertyToID("_SpecColor");
+        public static readonly int GlossMapScale = Shader.PropertyToID("_GlossMapScale");
+        public static readonly int Glossiness = Shader.PropertyToID("_Glossiness");
+
+        public static readonly int BaseMap = Shader.PropertyToID("_BaseMap");
+        public static readonly int BaseMapScaleTransform = Shader.PropertyToID("_BaseMap_ST"); // we dont support this yet
+        public static readonly int BaseMapRotation = Shader.PropertyToID("_BaseMapRotation"); // we dont support this yet
+        public static readonly int BaseMapUVs = Shader.PropertyToID("_BaseMapUVs");
+
+        public static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
+
+        public static readonly int NormalMapUVs = Shader.PropertyToID("_NormalMapUVs");
+        public static readonly int MetallicMapUVs = Shader.PropertyToID("_MetallicMapUVs");
+        public static readonly int EmissiveMapUVs = Shader.PropertyToID("_EmissiveMapUVs");
+
+        public static readonly int Metallic = Shader.PropertyToID("_Metallic");
+        public  static readonly int Smoothness = Shader.PropertyToID("_Smoothness");
+        public static readonly int Cutoff = Shader.PropertyToID("_Cutoff");
+
+        public  static readonly int BumpMap = Shader.PropertyToID("_BumpMap");
+        public  static readonly int BumpScale = Shader.PropertyToID("_BumpScale");
+        public   static readonly int BumpMapRotationPropId = Shader.PropertyToID("_BumpMapRotation"); // we dont support this yet
+        public  static readonly int BumpMapScaleTransformPropId = Shader.PropertyToID("_BumpMap_ST"); // we dont support this yet
+        public  static readonly int BumpMapUVChannelPropId = Shader.PropertyToID("_BumpMapUVChannel"); // we dont support this yet
+
+        public static readonly int OcclusionMap = Shader.PropertyToID("_OcclusionMap");
+        public static readonly int OcclusionStrength = Shader.PropertyToID("_OcclusionStrength");
+        public static readonly int OcclusionMapRotation = Shader.PropertyToID("_OcclusionMapRotation"); // we dont support this yet
+        public  static readonly int OcclusionMapScaleTransform = Shader.PropertyToID("_OcclusionMap_ST"); // we dont support this yet
+        public  static readonly int OcclusionMapUVChannel = Shader.PropertyToID("_OcclusionMapUVChannel"); // we dont support this yet
+
+        public static readonly int EmissionMap = Shader.PropertyToID("_EmissionMap");
+        public  static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
+        public  static readonly int EmissionMapRotation = Shader.PropertyToID("_EmissionMapRotation"); // we dont support this yet
+        public static readonly int EmissionMapScaleTransform = Shader.PropertyToID("_EmissionMap_ST"); // we dont support this yet
+        public static readonly int EmissionMapUVChannel = Shader.PropertyToID("_EmissionMapUVChannel"); // we dont support this yet
+
+        public static readonly int SrcBlend = Shader.PropertyToID("_SrcBlend");
+        public static readonly int DstBlend = Shader.PropertyToID("_DstBlend");
+        public static readonly int ZWrite = Shader.PropertyToID("_ZWrite");
+        public  static readonly int AlphaClip = Shader.PropertyToID("_AlphaClip");
+        public static readonly int Cull = Shader.PropertyToID("_Cull");
+    }
+}
