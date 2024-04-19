be3ba85aa07d250541acdf8416adbc68de96c428
Geoff Birch <geoff.birch@decentraland.org>
2024-04-19 11:27:55 +0100

    SceneShader metallicroughness fix

--- Runtime/Shaders/SceneRendering/Scene_Input.hlsl
+++ Runtime/Shaders/SceneRendering/Scene_Input.hlsl
@@ -43,7 +43,7 @@ TEXTURE2D(_MetallicGlossMap);   SAMPLER(sampler_MetallicGlossMap);
 
 half4 SampleMetallicSpecGloss(float2 uv, half albedoAlpha)
 {
-    half4 specGloss = half4(0.0, 0.0, 0.0, 0.0); //half4(SAMPLE_METALLICSPECULAR(uv));
+    half4 specGloss = half4(SAMPLE_METALLICSPECULAR(uv));
     #ifdef _SMOOTHNESS_TEXTURE_ALBEDO_CHANNEL_A
         specGloss.a = albedoAlpha * _Smoothness;
     #else
