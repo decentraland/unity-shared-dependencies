298aaa791bc74c1fadfe9f7182c8f90868dd80f2
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2026-03-25 21:32:13 +0900

    WebGPU compatibility (#84)
    * Reimplemented changes
    
    * Update Lit.shader.meta
    
    * Fixed issues with material generator

--- Runtime/GLTFastWrappers/DecentralandMaterialGenerator.cs
+++ Runtime/GLTFastWrappers/DecentralandMaterialGenerator.cs
@@ -1,9 +1,7 @@
-﻿using DCL.Helpers;
-using DCL.Shaders;
+﻿using DCL.Shaders;
 using GLTFast;
 using GLTFast.Materials;
 using GLTFast.Schema;
-using System;
 using UnityEngine;
 using UnityEngine.Rendering;
 using GLTFastMaterial = GLTFast.Schema.Material;
@@ -13,31 +11,30 @@ namespace DCL.GLTFast.Wrappers
 {
     public class DecentralandMaterialGenerator : MaterialGenerator
     {
+        private static readonly int SURFACE = Shader.PropertyToID("_Surface");
+
         // Historically we have no data on why we have this intensity
         private const float EMISSIVE_HDR_INTENSITY = 5f;
 
-        private readonly Shader _shader;
-        private readonly bool _preserveMaxAlpha;
+        private readonly Shader shader;
+        private readonly bool preserveMaxAlpha;
 
         private Material material;
 
         public DecentralandMaterialGenerator(string shaderName, bool preserveMaxAlpha = false)
         {
-            _preserveMaxAlpha = preserveMaxAlpha;
-            _shader = Shader.Find(shaderName);
+            this.preserveMaxAlpha = preserveMaxAlpha;
+            shader = Shader.Find(shaderName);
         }
 
-        protected override Material GenerateDefaultMaterial(bool pointsSupport = false) => new (_shader);
+        protected override Material GenerateDefaultMaterial(bool pointsSupport = false) => new (shader);
 
         /// <summary>
         /// Here we convert a GLTFMaterial into our Material using our shaders
         /// </summary>
-        /// <param name="gltfMaterial"></param>
-        /// <param name="gltf"></param>
-        /// <returns></returns>
         public override Material GenerateMaterial(int materialIndex, GLTFastMaterial gltfMaterial, IGltfReadable gltf, bool pointsSupport = false)
         {
-            material = new Material(_shader);
+            material = new Material(shader);
 
             SetMaterialName(materialIndex, gltfMaterial);
 
@@ -242,7 +239,7 @@ namespace DCL.GLTFast.Wrappers
                     material.SetInt(ShaderUtils.SrcBlend, (int)BlendMode.One);
                     material.SetInt(ShaderUtils.DstBlend, (int)BlendMode.Zero);
                     material.SetInt(ShaderUtils.ZWrite, 1);
-                    material.SetInt("_Surface", 1);
+                    material.SetInt(SURFACE, 1);
                     material.SetFloat(ShaderUtils.AlphaClip, 1);
                     material.EnableKeyword(ShaderUtils.KEYWORD_ALPHA_TEST);
                     material.DisableKeyword(ShaderUtils.KEYWORD_ALPHA_PREMULTIPLY);
@@ -250,8 +247,8 @@ namespace DCL.GLTFast.Wrappers
 
                     if (material.HasProperty(ShaderUtils.Cutoff))
                         material.SetFloat(ShaderUtils.Cutoff, alphaCutoff);
-                    
-                    if (_preserveMaxAlpha)
+
+                    if (preserveMaxAlpha)
                     {
                         material.SetInt(ShaderUtils.SrcBlendAlpha, (int)BlendMode.One);
                         material.SetInt(ShaderUtils.DstBlendAlpha, (int)BlendMode.One);
@@ -265,13 +262,13 @@ namespace DCL.GLTFast.Wrappers
                     material.SetInt(ShaderUtils.SrcBlend, (int)BlendMode.SrcAlpha);
                     material.SetInt(ShaderUtils.DstBlend, (int)BlendMode.OneMinusSrcAlpha);
                     material.SetInt(ShaderUtils.ZWrite, 0);
-                    material.SetInt("_Surface", 1);
+                    material.SetInt(SURFACE, 1);
                     material.DisableKeyword(ShaderUtils.KEYWORD_ALPHA_TEST);
                     material.EnableKeyword(ShaderUtils.KEYWORD_ALPHA_PREMULTIPLY);
                     material.renderQueue = (int)RenderQueue.Transparent;
                     material.SetFloat(ShaderUtils.Cutoff, 0);
-                    
-                    if (_preserveMaxAlpha)
+
+                    if (preserveMaxAlpha)
                     {
                         material.SetInt(ShaderUtils.SrcBlendAlpha, (int)BlendMode.One);
                         material.SetInt(ShaderUtils.DstBlendAlpha, (int)BlendMode.One);
